a6811815ccc8dbb72e7a3ec522756211d81baa2d
kartik kumar <gtgkartik@gmail.com>
2023-02-10 10:11:19 +0530

    Added question 6

--- FDA Lab/FDA Lab 3 User Defined Functions.R
+++ FDA Lab/FDA Lab 3 User Defined Functions.R
@@ -34,9 +34,40 @@ return_multiple_values(10,20)
 
 subtract_function <- function(x, y) { 
   # do something with x and y
-  z <- do.call("add_function", list(x+y) 
+  z <- do.call("add_function", list(x-y)) 
   # do something with z 
 }
 #In the above function we are returning the value of x and y and passing it to 
 # out new function named other function !
 subtract_function(2,3)
+
+# How do you handle errors within a user defined function in R?
+
+my_function5 <- function(x) { 
+  result <- tryCatch({ 
+    10/x
+  }, error = function(e) { 
+    # handle the error 
+  }) 
+ return (result *3)
+}
+
+my_function(4)
+
+# What is the difference between a user defined function and a built-in function in R?
+
+#+ The main difference between a user defined function and a built-in function
+#+in R is that a user defined function is written by the user, while a built-in
+#+function is already stored in the R language and can be called directly.
+#+User defined functions can be customized to do any task, while built-in
+#+functions are limited to the tasks they are designed to do. Additionally,
+#+user defined functions can accept any number of arguments, while built-in 
+#functions typically have a fixed number of arguments that they accept.
+#+Finally, user defined functions may be more efficient than built-in 
+#+functions as they can be optimized for the specific task they are designed to do.
+
+
+# How do you debug a user defined function in R?
+
+
+
